92ee88056c0a40e4b20fb0d41a352826552dc249
Akash suman das <48758204+akashdas123@users.noreply.github.com>
2023-09-20 22:54:57 +0530

    Update app.py

--- COPPERANANLYSIS/app.py
+++ COPPERANANLYSIS/app.py
@@ -151,5 +151,3 @@ with tab2:
             st.write('## :green[The Status is Won] ')
         else:
             st.write('## :red[The status is Lost] ')
-
-st.write( f'<h6 style="color:rgb(0, 153, 153,0.35);">App Created by TulasiNND</h6>', unsafe_allow_html=True )
